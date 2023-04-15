b7e2eacf595e248860a9a3309150522ede982ed9
Egelund48 <79721682+Egelund48@users.noreply.github.com>
2023-04-15 16:29:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hjælp.dk</title>
+        <link rel="stylesheet" href="style.css" type="text/css"/>
+    </head>
+    
+    <div class="header">
+        <h1> <a href="index.php">Hjælp.dk</a></h1>
+    </div>
+    <div class="navbar"> 
+        <a href="søgefelt.php">Søgefelt.php</a>
+    </div>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
