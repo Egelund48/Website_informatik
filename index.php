8022f8984c7103ca48f5b1745c8a535fb11e4de9
Egelund48 <79721682+Egelund48@users.noreply.github.com>
2023-04-18 20:00:00 +0200

    index.php
    Update

--- index.php
+++ index.php
@@ -1,25 +1,32 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<html lang="en">
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Hjælp.dk</title>
-        <link rel="stylesheet" href="style.css" type="text/css"/>
+        <link rel="stylesheet" href="style.css"/>
     </head>
     
     <div class="header">
-        <h1> <a href="index.php">Hjælp.dk</a></h1>
+        <h1> <a href="index.php" style="text-decoration: none;" style="text-align: center;">Hjælp.dk</a></h1>
     </div>
-    <div class="navbar"> 
-        <a href="søgefelt.php">Søgefelt.php</a>
-    </div>
+    
     <body>
+        <div class="navbar">
+            <li><a href="kontakt.php">Kontakt</a></li>
+            <li><a href="profil.php">Profil</a></li>
+            <li><a href="om_os.php">Om os</a></li>
+        </div>
         <?php
         // put your code here
         ?>
+        <div style="height:600px; margin-left: 300px; margin-right: 25%; margin-top: 5%;">
+            <h1>Landing page</h1>
+            <p style="line-height: 2.0; font-size:20px;">
+                Dette er landing page  
+            </p>
+        </div>
     </body>
 </html>
