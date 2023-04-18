4d62e49019731e032034f877cecdd506f9f9070e
Egelund48 <79721682+Egelund48@users.noreply.github.com>
2023-04-18 20:01:44 +0200

    Add files via upload

---
+++ profil.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hjælp.dk</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    
+    <div class="header">
+        <h1> <a href="index.php" style="text-decoration: none;" style="text-align: center;">Hjælp.dk</a></h1>
+    </div>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div class="navbar">
+            <li><a href="kontakt.php">Kontakt</a></li>
+            <li><a href="profil.php">Profil</a></li>
+            <li><a href="om_os.php">Om os</a></li>
+        </div>
+    <div style="height:600px; margin-left: 300px; margin-right: 25%; margin-top: 5%;">
+        <h1>Din Profil</h1>
+        <p style="line-height: 2.0; font-size:20px;">
+            Her er info om din profil.   
+        </p>
+    </div>
+    </body>
+</html>
