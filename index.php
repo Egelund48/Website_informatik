bec30e961eae26b07d24a045e3eb8dcbd94a72bf
Egelund48 <79721682+Egelund48@users.noreply.github.com>
2023-04-23 19:45:55 +0200

    lille update fra 23/4/2023

--- index.php
+++ index.php
@@ -10,14 +10,17 @@
     </head>
     
     <div class="header">
-        <h1> <a href="index.php" style="text-decoration: none;" style="text-align: center;">Hjælp.dk</a></h1>
+        <h1> <a class = "active"href="index.php" style="text-decoration: none;" style="text-align: center">Hjælp.dk</a></h1>
     </div>
     
     <body>
         <div class="navbar">
-            <li><a href="kontakt.php">Kontakt</a></li>
+            <li><a href="browse.php">Browse</a></li>
             <li><a href="profil.php">Profil</a></li>
+            <li><a href="kontakt.php">Kontakt</a></li>
             <li><a href="om_os.php">Om os</a></li>
+            <li ><a href="login.php">Login/Sign up </a></li>
+            <input class = "topnav" type="text" placeholder="Søg..">
         </div>
         <?php
         // put your code here
@@ -25,7 +28,55 @@
         <div style="height:600px; margin-left: 300px; margin-right: 25%; margin-top: 5%;">
             <h1>Landing page</h1>
             <p style="line-height: 2.0; font-size:20px;">
-                Dette er landing page  
+                Dette er landing page.
+                <br>
+
+                To-do liste over ting vi skal. 
+                <br>
+                <div>
+                <label for="option1">Lav et login system</label>
+                <input type="checkbox" id="option1">
+                </div>
+                <div>
+                <label for="option2">Lav et kontakt system</label>
+                <input type="checkbox" id="option2">
+                </div>
+                <div>
+                <label for="option3">Gør hjemmesiden pænere</label>
+                <input type="checkbox" id="option3">
+                </div>
+
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+                <script src="http://cdn.jsdelivr.net/jquery.cookie/1.4.0/jquery.cookie.min.js"></script>
+
+                <script>
+                $("#checkAll").on("change", function() {
+                    $(':checkbox').not(this).prop('checked', this.checked);
+                });
+
+                $(":checkbox").on("change", function(){
+                    var checkboxValues = {};
+                    $(":checkbox").each(function(){
+                    checkboxValues[this.id] = this.checked;
+                    });
+                    $.cookie('checkboxValues', checkboxValues, { expires: 7, path: '/' })
+                });
+
+                function repopulateCheckboxes(){
+                    var checkboxValues = $.cookie('checkboxValues');
+                    if(checkboxValues){
+                    Object.keys(checkboxValues).forEach(function(element) {
+                        var checked = checkboxValues[element];
+                        $("#" + element).prop('checked', checked);
+                    });
+                    }
+                }
+
+                $.cookie.json = true;
+                repopulateCheckboxes();
+                </script>
+                <br>
+                Tilføj mere løbende...
             </p>
         </div>
     </body>
